4ae47f7b98832f59ac82b4ce52f778d6e0cc5de2
ashu02-git <shuhei.abe1107@gmail.com>
2021-06-01 16:29:29 +0900

    ユーザ情報ファイルにテキストファイルに出力

--- regist-user/routes/registUser.js
+++ regist-user/routes/registUser.js
@@ -1,11 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const fs = require("fs");
+
+const databasePath = 'user.txt'
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
   var name = req.body.name;
   var age = req.body.age;
+  const data = `${name} , ${age} \n`;
+  const options = {
+    flag: 'a'
+  };
 
+  fs.writeFile(databasePath, data, options, (err) => {
+    if(err) console.log(err);
+    else console.log('write end');
+  });
+  
   res.render('registUser', { 
     title: 'ユーザー作成完了',
     name: name,
