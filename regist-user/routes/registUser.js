a04420b20ffa3b78f441b2200af7d865a5fd9ec0
ashu02-git <shuhei.abe1107@gmail.com>
2021-06-01 18:00:07 +0900

    promise表記でファイル書き出し

--- regist-user/routes/registUser.js
+++ regist-user/routes/registUser.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const fs = require("fs");
+const promiseFs = require('fs').promises;
+
 const textPath = "./database/user.txt"
 
 
@@ -15,11 +17,22 @@ router.post('/', function(req, res, next) {
   };
 
 
-  fs.writeFile(textPath, data, options, (err) => {
-    if(err) console.log(err);
-    else console.log('write end');
-  });
+  // fs.writeFile(textPath, data, options, (err) => {
+  //   if(err) console.log(err);
+  //   else console.log('write end');
+  // });
 
+  // promise表記
+  promiseFs
+    .writeFile(textPath, data, options)
+      .then((data) =>{
+        console.log('ユーザー情報保存完了')
+      })
+      .catch((err) => {
+        console.log(err);
+        console.log('catch end');
+      });
+      // console.log('function end');
 
   res.render('registUser', { 
     title: 'ユーザー作成完了',
